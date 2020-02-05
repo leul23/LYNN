9eaf7ffb7260c8d5222fb0a220cb32afe529ccdc
Leul Mebratu <leulmebratu@gmail.com>
2020-02-05 21:09:47 +0300

    Registration

--- Register/forms.py
+++ Register/forms.py
@@ -3,8 +3,10 @@ from django import forms
 from django.contrib.auth.models import User
 
 class UserRegistration(UserCreationForm):
-    email = forms.EmailField()
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
-        
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
--- Register/views.py
+++ Register/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistration
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
 def register(request):
-    return render (request,'Register/index.html')
+    if request.method == "POST":
+        form = UserRegistration(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('Home:index')
+    else:
+        form = UserRegistration()
+    return render (request,'Register/index.html',{'form' : form})
