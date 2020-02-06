3d58a4222b58f086072541688bcff2707740fcb7
Nardos Zebene <zebenenardosabi@gmail.com>
2020-02-06 16:05:57 +0300

    final

--- Request/admin.py
+++ Request/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-# from .models import requests
+from .models import requests
 
 # Register your models here.
 
-#  admin.site.register(requests)
+admin.site.register(requests)
--- Request/forms.py
+++ Request/forms.py
@@ -6,8 +6,8 @@ class RequestForm(forms.ModelForm):
     username = forms.CharField(max_length=50, required=False, help_text='Enter your name.')
     location = forms.CharField(max_length=50, required=False, help_text='Enter the description of the location briefly.')
     request = forms.CharField(max_length=50, required=False, help_text='Enter the description of the problem briefly.')
-    phone = forms.CharField(max_length=50, required=False, help_text='Enter appropriate phone number.')
+    phonenumber = forms.IntegerField(required=False, help_text='Enter appropriate phone number.')
 
     class Meta:
         model = requests
-        fields = ('username', 'location', 'request', 'phone', )
+        fields = ('username', 'location', 'request', 'phonenumber', )
